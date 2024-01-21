810493b9a38fe1d9f6131ca7dd13d6b937eb8982
Ryan Burnside <pixeloutlaw@gmail.com>
2024-01-20 23:28:25 -0700

    Added in support for opcodes, still work to be done...

--- lmc.lisp
+++ lmc.lisp
@@ -17,12 +17,19 @@
   (assert (<= 0 opcode 99))
   (mod opcode 100))
 
+(defun opcodep (num)
+  "Determines if the opcode is legal."
+  (or (zerop num)
+      (<= 100 num 899)
+      (member '(910 902))))
+
 (defun do-opcode (num)
   "Performs an action given an instruction.
 Where O is the operation and XX is the mailbox index.
-In the form of OXX."
-  (if (<= 0 num 999)
+In the form of OXX. Steps 4 and 5 in the instructions."
+  (if (opcodep num)
       (let ((mailbox (mailbox-from-opcode num)))
+        (incf *instruction-counter*)
         (cond
           ((zerop num) (stop))
           ((<= 100 num 199) (add mailbox))
@@ -30,12 +37,12 @@ In the form of OXX."
           ((<= 300 num 399) (sto mailbox))
           ((<= 400 num 499) (sta mailbox))
           ((<= 500 num 599) (lod mailbox))
-          ((<= 600 num 699) 'B)
-          ((<= 700 num 799) 'BZ)
-          ((<= 800 num 899) 'BP)
-          ((= 910 num) 'READ)
-          ((= 902 num) 'PRINT)))
-      (format t "ERROR opcode ~a is not between 0 and 999!" num)))
+          ((<= 600 num 699) (b mailbox))
+          ((<= 700 num 799) (bz mailbox))
+          ((<= 800 num 899) (bp mailbox))
+          ((= 910 num) (red))
+          ((= 902 num) (prt))))
+      (format t "Bad opcode: ~a. Ignored." num)))
 
 (defun set-mailbox-f (mailbox value)
   "Setter for the *mailboxes* array."
@@ -78,3 +85,36 @@ into the address portion of the instruction in mailbox xx."
 (defun lod (mailbox)
   "Loads the contents of mailbox xx into the calculator."
   (setf *calculator* (get-mailbox mailbox)))
+
+(defun b (mailbox)
+  "This instruction sets the instruction counter to the number xx,
+thus effectively branching to mailbox xx See the note for instruction
+BP"
+  (setf *instruction-count* mailbox))
+
+(defun bz (mailbox)
+  "IF the calculator value is zero, THEN set the instruction counter
+to the number xx, thus effectively branching to mailbox xx. See the
+ note for instruction BP"
+  (when (zerop *calculator*)
+    (setf *instruction-count* mailbox)))w
+
+(defun bp (mailbox)
+  "IF the calculator value is positive, THEN set the instruction
+ counter to the number xx, thus effectively branching to mailbox xx.
+ NOTE: zero is considered positive."
+  (when (>= *calculator* 0)
+    (setf *instruction-count* mailbox)))
+
+(defun red ()
+  (setf *calculator* (pop *inbox*)))
+
+(defun prt ()
+  (setf *outbox*
+        (reverse (cons *calculator* (reverse *outbox*)))))
+
+(defun main ()
+  (while (not *finished*)
+         ;; Goto mailbox with instruction counter
+         (let ((mailbox-value (get-mailbox *instruction-counter*)))
+           (do-opcode mailbox-value))))
